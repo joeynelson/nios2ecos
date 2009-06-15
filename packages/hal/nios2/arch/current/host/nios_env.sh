be3a9c298687e19b3e99dacef65504bb414f720a
edgar <edgar@2333b21a-1c27-0410-8132-afba56cb5048>
2009-06-15 11:29:39 +0000

    /nios2ecos/packages/hal/nios2/arch/current/host/nios_env.sh and 	/nios2ecos/packages/hal/nios2/arch/current/host/nios2ecosconfig Linux friendly modifications
    git-svn-id: http://www.ecosforge.net/ecosforge/trunk/nios2ecos@217 2333b21a-1c27-0410-8132-afba56cb5048

--- packages/hal/nios2/arch/current/host/nios_env.sh
+++ packages/hal/nios2/arch/current/host/nios_env.sh
@@ -6,8 +6,14 @@
 # Quartus ships with an aging Cygwin version
 # 
 # See setup.sh
-export CYGWIN=nontsec
+
+if [ "`uname`" != "Linux" ] then
+	export CYGWIN=nontsec
+fi
+
 export NIOS_ECOS=`pwd`/../../nios2ecos/packages
+
+if [ "`uname`" != "Linux" ] then
 
 # DANGER!!! here we need windows-like paths for compatibility.
 export QUARTUS_ROOTDIR=c:/altera/81/quartus
@@ -21,7 +27,20 @@ export PATH=$PATH:/cygdrive/c/altera/81/quartus/sopc_builder/bin
 export PATH=$PATH:/cygdrive/c/altera/81/quartus/bin
 export PATH=$PATH:/cygdrive/c/altera/81/nios2eds/bin/nios2-gnutools/H-i686-pc-cygwin/bin
 
+else
+
+export QUARTUS_ROOTDIR=/opt/altera9.0/quartus
+export SOPC_KIT_NIOS2=/opt/altera9.0/nios2eds
+export PERL5LIB=/usr/bin:/opt/altera9.0/quartus/sopc_builder/bin/perl_lib:/opt/altera9.0/quartus/sopc_builder/bin/europa:/opt/altera9.0/quartus/sopc_builder/bin
+
+# generally place the altera stuff *LAST* in the path because it contains
+# lots of obsolete stuff
+export PATH=$PATH:$NIOS_ECOS/hal/nios2/arch/current/host
+export PATH=$PATH:/opt/altera9.0/quartus/sopc_builder/bin
+export PATH=$PATH:/opt/altera9.0/quartus/bin
+
+fi
 
 # set up a list of repositories. The NIOS_ECOS repository should be first.
 # where a,b,c are cygwin paths to other eCos repositories.
-export ECOS_REPOSITORY=$NIOS_ECOS:a:b:c
+# export ECOS_REPOSITORY=$NIOS_ECOS:a:b:c
