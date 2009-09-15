582b74fabab45df133720efb1bb641a4b344ce64
oyvind <oyvind@2333b21a-1c27-0410-8132-afba56cb5048>
2009-09-15 10:32:40 +0000

    2009-09-15 Øyvind Harboe <oyvind.harboe@zylin.com>
    	* fix nios_env.sh script (do not use set -e), still wip...
    	* updated compat/posix committed until libstdc++ pthread 
    	exceptions are tested & working
    
    
    
    git-svn-id: http://www.ecosforge.net/ecosforge/trunk/nios2ecos@257 2333b21a-1c27-0410-8132-afba56cb5048

--- nios_env.sh
+++ nios_env.sh
@@ -5,29 +5,39 @@
 #
 # Intended to work on Cygwin & Linux out of the box(tested on Ubuntu 9.04
 # as of writing).
-set -e
-export CYGWIN=nontsec
-export NIOS_ECOS=`pwd`/packages
 
+export NIOS_ECOS=`pwd`/packages
 
 if [ `uname` = Linux ] ;then
 	echo "Linux box"
-	export WIN_ALTERA_ROOTDIR=/opt/altera9.0/
-	export CYG_ALTERA_ROOTDIR=/opt/altera9.0/
+	export WIN_ALTERA_ROOTDIR=/opt/altera9.0
+	export CYG_ALTERA_ROOTDIR=/opt/altera9.0
 	export PATH=$PATH:$CYG_ALTERA_ROOTDIR/nios2eds/bin/nios2-gnutools/H-i686-pc-linux-gnu/bin
 	export TMP=/tmp
 else
 	echo "Cygwin"
+	export CYGWIN=nontsec
 	export WIN_ALTERA_ROOTDIR=c:/altera/90
 	export CYG_ALTERA_ROOTDIR=/cygdrive/c/altera/90
 	export PATH=$PATH:$CYG_ALTERA_ROOTDIR/nios2eds/bin/nios2-gnutools/H-i686-pc-cygwin/bin
 fi
 
-
 # DANGER!!! here we need windows-like paths for compatibility.
 export QUARTUS_ROOTDIR=$WIN_ALTERA_ROOTDIR/quartus
 export SOPC_KIT_NIOS2=$WIN_ALTERA_ROOTDIR/nios2eds
 export PERL5LIB=/bin:$CYG_ALTERA_ROOTDIR/quartus/sopc_builder/bin/perl_lib:$CYG_ALTERA_ROOTDIR/quartus/sopc_builder/bin/europa:$CYG_ALTERA_ROOTDIR/quartus/sopc_builder/bin
+
+# This is the standard installation directory for eCos 3.0
+echo "Adding eCos 3.0 tools to path: ~/ecos/ecos-3.0/tools/bin"
+# FIX!!! check if ecosconfig is already in the path and only add
+# this to path if it isn't 
+export PATH=$PATH:~/ecos/ecos-3.0/tools/bin
+# FIX!!! check if ECOS_REPOSITORY variable is already set up
+export ECOS_REPOSITORY=~/ecos/ecos-3.0/packages
+echo "Prepend Nios eCos repository to ECOS_REPOSITORY=$ECOS_REPOSITORY"
+export ECOS_REPOSITORY=$NIOS_ECOS:$ECOS_REPOSITORY
+export ECOS_REPOSITORY=$NIOS_ECOS/../tools/gcc4libstdxx/ecos:$ECOS_REPOSITORY
+echo "ECOS_REPOSITORY=$ECOS_REPOSITORY"
 
 
 # generally place the altera stuff *LAST* in the path because it contains
@@ -37,14 +47,9 @@ export PATH=$PATH:$CYG_ALTERA_ROOTDIR/quartus/sopc_builder/bin
 export PATH=$PATH:$CYG_ALTERA_ROOTDIR/quartus/bin
 export PATH=$PATH:$CYG_ALTERA_ROOTDIR/nios2eds/bin
 
-#export PATH=$CYG_ALTERA_ROOTDIR/nios2eds/bin:$CYG_ALTERA_ROOTDIR/nios2eds/sdk2/bin:$CYG_ALTERA_ROOTDIR/nios2eds/bin/fs2/bin:$CYG_ALTERA_ROOTDIR/quartus/win:$CYG_ALTERA_ROOTDIR/quartus/bin:$CYG_ALTERA_ROOTDIR/quartus/bin/perl/bin:$CYG_ALTERA_ROOTDIR/quartus/bin/gnu:$PATH
 
 
-
-export ECOS_REPOSITORY=$NIOS_ECOS:`pwd`/../ecos/packages
-
-
+# Workaround for cygpath problems.
 if [ `uname` = Linux ] ;then
-	# Workaround for cygpath problems.
-	export PATH=$PATH:$NIOS_ECOS/hal/nios2/arch/current/host/cygpath	
+	export PATH=$PATH:$NIOS_ECOS/hal/nios2/arch/current/host/cygpath
 fi
