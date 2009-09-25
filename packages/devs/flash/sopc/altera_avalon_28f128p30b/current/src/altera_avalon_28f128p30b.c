e7259ecdfe35e6bbe08e47d8c3e2749c242f45fe
laurentiu <laurentiu@2333b21a-1c27-0410-8132-afba56cb5048>
2009-09-25 14:08:46 +0000

    Added a global option in $NIOS_ECOS/hal/nios2/arch/current/cdl/hal_nios2.cdl CYGBLD_GLOBAL_START_ADDRESS, to set the absolute address of the start address in memory configuration Replaced in $NIOS_ECOS/hal/nios2/arch/current/gtf/mlt_nios2.ldi.gtf %BASE_ADDRESS% with CYGBLD_GLOBAL_START_ADDRESS Added flash driver for Altera Avalon 28F128P30B Fixed some copyright texts
    git-svn-id: http://www.ecosforge.net/ecosforge/trunk/nios2ecos@284 2333b21a-1c27-0410-8132-afba56cb5048

--- packages/devs/flash/sopc/altera_avalon_28f128p30b/current/src/altera_avalon_28f128p30b.c
+++ packages/devs/flash/sopc/altera_avalon_28f128p30b/current/src/altera_avalon_28f128p30b.c
@@ -1,8 +1,8 @@
 //# ====================================================================
 //#
-//#      altera_avalon_28f256p30b.c
+//#      altera_avalon_28f128p30b.c
 //#
-//#      28F256P30B flash driver.
+//#      28F128P30B flash driver.
 //#
 //# ====================================================================
 //#####ECOSGPLCOPYRIGHTBEGIN####
@@ -43,10 +43,10 @@
 #include <cyg/io/strata_dev.h>
 #include <cyg/hal/system.h>
 
-//28F256P30B
+//28F128P30B
 
 static const CYG_FLASH_FUNS(
-	altera_avalon_28f256p30b_flash_strata_funs,
+	altera_avalon_28f128p30b_flash_strata_funs,
     &cyg_strata_init_check_devid_16,
     &cyg_flash_devfn_query_nop,
     &cyg_strata_erase_16,
@@ -55,24 +55,24 @@ static const CYG_FLASH_FUNS(
     &cyg_strata_lock_k3_16,
     &cyg_strata_unlock_k3_16);
 
-static const cyg_strata_dev altera_avalon_28f256p30b_flash_priv = {
+static const cyg_strata_dev altera_avalon_28f128p30b_flash_priv = {
     .manufacturer_code = CYG_FLASH_STRATA_MANUFACTURER_INTEL,
-    .device_code = 0x891C,
+    .device_code = 0x881B,
     .bufsize    = 16,
     .block_info = {
         { 0x00008000, 4 	},
-        { 0x00020000, 255	}
+        { 0x00020000, 127	}
     }
 };
 
-CYG_FLASH_DRIVER(altera_avalon_28f256p30b_flash,
-                 &altera_avalon_28f256p30b_flash_strata_funs,
+CYG_FLASH_DRIVER(altera_avalon_28f128p30b_flash,
+                 &altera_avalon_28f128p30b_flash_strata_funs,
                  0,
                  EXT_FLASH_BASE,
                  (EXT_FLASH_BASE + EXT_FLASH_SPAN - 1),
                  2,
-                 altera_avalon_28f256p30b_flash_priv.block_info,
-                 &altera_avalon_28f256p30b_flash_priv
+                 altera_avalon_28f128p30b_flash_priv.block_info,
+                 &altera_avalon_28f128p30b_flash_priv
 );
 
 #endif //CYGPKG_DEVS_FLASH_STRATA_V2
