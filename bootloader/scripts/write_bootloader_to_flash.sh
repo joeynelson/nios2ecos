f1b01c3c988eacf1a6e75f35fd207365152d6322
oyvind <oyvind@2333b21a-1c27-0410-8132-afba56cb5048>
2009-12-18 19:19:07 +0000

    bootloader: remove unused files
    some specialized build files were committed.
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>
    
    git-svn-id: http://www.ecosforge.net/ecosforge/trunk/nios2ecos@321 2333b21a-1c27-0410-8132-afba56cb5048

--- bootloader/scripts/write_bootloader_to_flash.sh
+++
@@ -1,7 +0,0 @@
-. ./scripts/set_params.sh
-
-bin2flash --location=$FACTORY_SOF_OFFSET --input=$BOOTLOADER_UPGRADE.bin --output=$BOOTLOADER_UPGRADE.srec
-
-nios2-flash-programmer --base=$FLASH_BASE $BOOTLOADER_UPGRADE.srec
-
-
